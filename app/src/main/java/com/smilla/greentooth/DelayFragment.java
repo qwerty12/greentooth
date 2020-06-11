7e1c8b971b204ff68697075e84e3ba7859c20cb0
Nicklas Bergman <nicklas.bergman@gmail.com>
2020-06-11 08:51:29 +0200

    * Fixed current NumberPicker values not being saved and restored when switching activities

--- app/src/main/java/com/smilla/greentooth/DelayFragment.java
+++ app/src/main/java/com/smilla/greentooth/DelayFragment.java
@@ -8,10 +8,10 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
@@ -29,66 +29,52 @@ public class DelayFragment extends DialogFragment {
     DelayInterface delayInterface;
 
     @Override
+    @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Activity activity = getActivity();
         LayoutInflater layoutInflater = LayoutInflater.from(activity);
-        View dialogView = layoutInflater.inflate(R.layout.delay_dialog, null);
-        AlertDialog dialog = new AlertDialog.Builder(activity)
-                .setNegativeButton(R.string.cancel, null)
-                .setPositiveButton(R.string.okay, null)
-                .create();
-        dialog.setView(dialogView);
-        return dialog;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
+        View view = layoutInflater.inflate(R.layout.delay_dialog, null);
+        NumberPicker minutesPicker = view.findViewById(R.id.minutesPicker);
+        NumberPicker secondsPicker = view.findViewById(R.id.secondsPicker);
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences(APP_KEY, 0);
-        AlertDialog dialog = (AlertDialog) getDialog();
-        Button okButton = dialog.getButton(Dialog.BUTTON_POSITIVE);
-        Button cancelButton = dialog.getButton(Dialog.BUTTON_NEGATIVE);
-        NumberPicker minutesPicker = dialog.findViewById(R.id.minutesPicker);
-        NumberPicker secondsPicker = dialog.findViewById(R.id.secondsPicker);
-
         minutesPicker.setMaxValue(GreenApplication.MAX_MINUTE_DELAY);
         minutesPicker.setMinValue(GreenApplication.MIN_MINUTE_DELAY);
         secondsPicker.setMaxValue(GreenApplication.MAX_SECOND_DELAY);
         secondsPicker.setMinValue(GreenApplication.MIN_SECOND_DELAY);
-
         //Show 01 instead of 1, etc...
         NumberPicker.Formatter formatter = value -> String.format(Locale.getDefault(), "%02d", value);
         minutesPicker.setFormatter(formatter);
         secondsPicker.setFormatter(formatter);
-
-        //Get child EditTexts
+        int currentDelay = Util.getSaneDelay(sharedPreferences);
+        if (currentDelay > 0) {
+            int minutes = currentDelay / 60;
+            int seconds = currentDelay % 60;
+            minutesPicker.setValue(minutes);
+            secondsPicker.setValue(seconds);
+        }
+        /* Getting the EditText children and extracting the text from them allows the user to not have to
+           press "done" on the software keyboard to save their changes. */
         int textId = Resources.getSystem().getIdentifier("numberpicker_input", "id", "android");
         EditText minutesText = minutesPicker.findViewById(textId);
         EditText secondsText = secondsPicker.findViewById(textId);
 
-        okButton.setOnClickListener((dialogInterface) -> {
-            minutesPicker.setValue(Integer.parseInt(minutesText.getText().toString()));
-            secondsPicker.setValue(Integer.parseInt(secondsText.getText().toString()));
-            int delay = minutesPicker.getValue() * 60 + secondsPicker.getValue();
-            sharedPreferences.edit().putInt(DELAY_KEY, delay).apply();
-            delayInterface.updateDelayText();
-            dialog.dismiss();
-        });
-        cancelButton.setOnClickListener((dialogInterface) -> {
-            dialog.dismiss();
-        });
-
-        int delay = Util.getSaneDelay(sharedPreferences);
-        if (delay > 0) {
-            int minutes = delay / 60;
-            int seconds = delay % 60;
-            minutesPicker.setValue(minutes);
-            secondsPicker.setValue(seconds);
-        }
+        AlertDialog alertDialog = new AlertDialog.Builder(activity)
+                .setPositiveButton(R.string.okay, (dialog, which) -> {
+                    minutesPicker.setValue(Integer.parseInt(minutesText.getText().toString()));
+                    secondsPicker.setValue(Integer.parseInt(secondsText.getText().toString()));
+                    int newDelay = minutesPicker.getValue() * 60 + secondsPicker.getValue();
+                    sharedPreferences.edit().putInt(DELAY_KEY, newDelay).apply();
+                    delayInterface.updateDelayText();
+                    dialog.dismiss();
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .create();
+        alertDialog.setView(view);
+        return alertDialog;
     }
 
     @Override
-    public void onAttach(Context context) {
+    public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         try {
             delayInterface = (DelayInterface) context;
